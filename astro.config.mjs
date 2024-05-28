a0e96ecc91519811fefd6efaaf661476327fbf29
Joseph Fridye <jfridye@gmail.com>
2024-05-28 08:35:31 -0500

    Theming. Vanilla JS interactivity.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://gregarious-starlight-d767cd.netlify.app/"
+  site: "https://gregarious-starlight-d767cd.netlify.app/",
+  integrations: [preact()]
 });
